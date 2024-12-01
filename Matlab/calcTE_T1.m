0554719c309be8f08473da338678e2896e453677
xylacon <65502587+xylacon@users.noreply.github.com>
2024-11-30 19:14:04 -0600

    Delete Matlab directory

--- Matlab/calcTE_T1.m
+++
@@ -1,5 +0,0 @@
-function TE = calcTE_T1(T2_WM)
-    % T2_WM = T2 relaxation time (WM) (milliseconds)
-    
-    TE = T2_WM / 8;
-end
--- Matlab/extractTissueMaps.m
+++
@@ -1,5 +0,0 @@
-function [GM, WM, CSF] = extractTissueMaps(patient)
-    GM = (patient == 1) | (patient == 2);
-    WM = (patient == 3);
-    CSF = (patient == 4);
-end
