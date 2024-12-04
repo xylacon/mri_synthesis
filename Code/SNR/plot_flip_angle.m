2406e93d7d4efe3555ac4218f8dae308934dc6fc
heyilly <128723269+heyilly@users.noreply.github.com>
2024-12-03 19:51:03 -0600

    Update plot_flip_angle.m

--- Code/SNR/plot_flip_angle.m
+++ Code/SNR/plot_flip_angle.m
@@ -1,10 +1,12 @@
-% plot Ctheta vs Theta %
-
 function plot_flip_angle(theta, contrast, optimal_alpha)
+    % Converts theta to degrees for plotting
     theta_deg = rad2deg(theta);
     figure;
-    plot(theta_deg, contrast, '-b', 'LineWidth', 2);
-    hold on; plot(optimal_alpha, max(contrast), 'ro', 'MarkerFaceColor', 'r');
-    xlabel('\theta [degrees]'); ylabel('C_\theta'); title('Contrast vs Flip Angle');
-    grid on; hold off;
+    plot(theta_deg, contrast, '-b', 'LineWidth', 2); % Plot contrast vs flip angle
+    hold on; plot(optimal_alpha, max(contrast), 'ro', 'MarkerFaceColor', 'r'); % Highlight optimal flip angle
+    xlabel('\theta [degrees]');
+    ylabel('C_\theta');
+    title('Contrast vs Flip Angle');
+    grid on;
+    hold off;
 end
